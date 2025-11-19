51cd916dfc9a168f353017936e095056cbc0d645
jiangtengjin <jiangtengjin@appiron.cn>
2025-11-19 19:46:44 +0800

    feat: 自定义 MCP 服务

---
+++ image-search-mcp-server/src/test/java/com/xhh/imagesearchmcpserver/ImageSearchMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhh.imagesearchmcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageSearchMcpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
